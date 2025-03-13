a1b06a64f974c001208a16e4379e59fc489ad24f
kiplaa <ekiplangat64@gmail.com>
2025-03-13 16:07:55 +0300

    adding static in production

--- EnoSoftwares/settings.py
+++ EnoSoftwares/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
