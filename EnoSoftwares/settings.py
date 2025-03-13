ab7654dbe22feca4791b361b86b02db0cd8ab2c8
kiplaa <ekiplangat64@gmail.com>
2025-03-13 15:52:01 +0300

    allowing hosts

--- EnoSoftwares/settings.py
+++ EnoSoftwares/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-93j-fht=o*il-wzu1j7#m#y7+@*-rq5v_9_gf%&22sob6s&0@&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
